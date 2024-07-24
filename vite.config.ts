6c8748ad46ebba5b9610f09280d143dcdc4bfc57
bondarev vlad <bondarev.vlad.01@gmail.com>
2024-07-24 16:49:45 +0300

    fix: change config

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,7 @@ import sass from 'sass'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['**/*.scss'],
-  },
   css: {
-    modules: {
-      localsConvention: 'camelCaseOnly',
-    },
     preprocessorOptions: {
       scss: {
         implementation: sass,
